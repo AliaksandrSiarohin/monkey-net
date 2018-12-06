Add different normalization. Fix matrix inversion. Changes in frames_dataset.

modules/kp_extractor.py
@@ -1,8 +1,8 @@
 from torch import nn
 import torch
 import torch.nn.functional as F
-from modules.util import Hourglass, make_coordinate_grid, matrix_inverse
-
+from modules.util import Hourglass, make_coordinate_grid, matrix_inverse, matrix_det
+import numpy as np
 
 def kp2gaussian(kp, spatial_size, kp_variance='matrix'):
     mean = kp['mean']
@@ -25,8 +25,8 @@ def kp2gaussian(kp, spatial_size, kp_variance='matrix'):
         var = kp['var']
         inv_var = matrix_inverse(var)
         shape = inv_var.shape[:number_of_leading_dimensions] + (1, 1, 2, 2)
-        var = inv_var.view(*shape)
-        under_exp = torch.matmul(torch.matmul(mean_sub.unsqueeze(-2), var), mean_sub.unsqueeze(-1))
+        inv_var = inv_var.view(*shape)
+        under_exp = torch.matmul(torch.matmul(mean_sub.unsqueeze(-2), inv_var), mean_sub.unsqueeze(-1))
         under_exp = under_exp.squeeze(-1).squeeze(-1)
         out = torch.exp(-0.5 * under_exp)
     elif kp_variance == 'single':
@@ -35,12 +35,7 @@ def kp2gaussian(kp, spatial_size, kp_variance='matrix'):
         out = torch.exp(-0.5 * (mean_sub ** 2).sum(-1) / kp_variance)
      
     out_shape = out.shape
-    out = out.view(out_shape[0], out_shape[1], out_shape[2], -1)
-    
-    heatmap = out / out.sum(dim=3, keepdim=True)
-    heatmap[torch.isnan(heatmap)] = 0
-    out = heatmap.view(*out_shape)
-
+    out = out / 1000
     return out
 
 
@@ -60,6 +55,7 @@ def gaussian2kp(heatmap, kp_variance='matrix'):
         var = var.sum(dim=(3, 4))
         var = var.permute(0, 2, 1, 3, 4)
         kp['var'] = var
+        
     elif kp_variance == 'single':
         mean_sub = grid - mean.unsqueeze(-2).unsqueeze(-2)
         var = mean_sub ** 2

modules/util.py
@@ -187,20 +187,11 @@ class Hourglass(nn.Module):
         return self.decoder(self.encoder(x))
 
 
-def matrix_inverse(batch_of_matrix, eps=1e-6):
-    init_shape = batch_of_matrix.shape
-    a = batch_of_matrix[..., 0, 0].unsqueeze(-1)
-    b = batch_of_matrix[..., 0, 1].unsqueeze(-1)
-    c = batch_of_matrix[..., 1, 0].unsqueeze(-1)
-    d = batch_of_matrix[..., 1, 1].unsqueeze(-1)
-
-    det = a * d - b * c
-    out = torch.cat([d, -b, -c, a], dim=-1)
-    eps = torch.tensor(eps).type(out.type())
-    out /= det.max(eps)
-
-    return out.view(init_shape)
-
+def matrix_inverse(batch_of_matrix):
+    b_mat = batch_of_matrix
+    eye = b_mat.new_ones(b_mat.size(-1)).diag().expand_as(b_mat)
+    b_inv, _ = torch.gesv(eye, b_mat)
+    return b_inv
 
 def matrix_det(batch_of_matrix):
     a = batch_of_matrix[..., 0, 0].unsqueeze(-1)

prediction.py
@@ -10,6 +10,7 @@ from modules.prediction_module import PredictionModule
 from augmentation import SelectRandomFrames, VideoToTensor
 from tqdm import trange
 from frames_dataset import FramesDataset
+from sync_batchnorm import  DataParallelWithCallback
 
 
 class KpDataset(Dataset):
@@ -41,6 +42,9 @@ def prediction(config, generator, kp_extractor, checkpoint, log_dir):
     else:
         raise AttributeError("Checkpoint should be specified for mode='prediction'.")
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
+
+    generator = DataParallelWithCallback(generator)
+    kp_extractor = DataParallelWithCallback(kp_extractor)
 
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -92,8 +96,6 @@ def prediction(config, generator, kp_extractor, checkpoint, log_dir):
             for k in x:
                 x[k][:, init_frames:] = 0
             prediction = predictor(x)
-            #prediction = {k: v[:, :-1] for k, v in prediction.items()}
-            #target = {k: v[:, 1:] for k, v in x.items()}
 
             loss = sum([torch.abs(gt[k][:, init_frames:] - prediction[k][:, init_frames:]).mean() for k in x])
 
@@ -123,7 +125,8 @@ def prediction(config, generator, kp_extractor, checkpoint, log_dir):
             kp_video = predictor(kp_init)
             for k in kp_video:
                 kp_video[k][:, :init_frames] = kp_init[k][:, :init_frames]
-            kp_video['var'] = kp_init['var'][:, (init_frames-1):init_frames].repeat(1, kp_video['var'].shape[1], 1, 1, 1)
+            if 'var' in kp_video:
+                kp_video['var'] = kp_init['var'][:, (init_frames-1):init_frames].repeat(1, kp_video['var'].shape[1], 1, 1, 1)
             out = {'video_prediction': [], 'video_deformed': []}
             for i in range(x['video_array'].shape[2]):
                      kp_target = {k: v[:, i:(i + 1)] for k, v in kp_video.items()}

reconstruction.py
@@ -30,8 +30,7 @@ def reconstruction(config, generator, kp_extractor, checkpoint, log_dir, dataset
 
     generator.eval()
     kp_extractor.eval()
-
-    
+  
     for it, x in tqdm(enumerate(dataloader)):
         if it == 1000:
            break
@@ -53,7 +52,7 @@ def reconstruction(config, generator, kp_extractor, checkpoint, log_dir, dataset
             out['kp_appearance'] = kp_appearance
 
             x['appearance_array'] = x['video_array'][:, :, :1]
-            
+             
             #Store to .png for evaluation
             out_video_batch = out['video_prediction'].data.cpu().numpy()
             out_video_batch = np.concatenate(np.transpose(out_video_batch, [0, 2, 3, 4, 1])[0], axis=1) 
@@ -67,5 +66,4 @@ def reconstruction(config, generator, kp_extractor, checkpoint, log_dir, dataset
                                        config['loss_weights']['reconstruction'][0])
             loss_list.append(loss.data.cpu().numpy())
             del x, kp_video, kp_appearance, out, loss
-
     print ("Reconstruction loss: %s" % np.mean(loss_list))

train.py
@@ -11,8 +11,7 @@ from torch.autograd import Variable
 import warnings
 import gc
 from sync_batchnorm import DataParallelWithCallback
-
-
+from functools import reduce
 
 def set_optimizer_lr(optimizer, lr):
     for param_group in optimizer.param_groups:
@@ -56,13 +55,14 @@ class GeneratorFullModel(torch.nn.Module):
                 discriminator_maps_deformed = self.discriminator(video_deformed, **kp_dict)
         else:
             discriminator_maps_deformed = None
+
+
+
         loss = generator_loss(discriminator_maps_generated=discriminator_maps_generated,
                                                          discriminator_maps_deformed=discriminator_maps_deformed,
                                                          discriminator_maps_real=discriminator_maps_real,
                                                          loss_weights=self.config['loss_weights'])
-        
-#        self.gen_values = gen_values
-#        self.gen_names = gen_names          
+
         return  tuple(loss) + (generated, kp_joined)
  
 
@@ -81,8 +81,6 @@ class DiscriminatorFullModel(torch.nn.Module):
         loss = discriminator_loss(discriminator_maps_generated=discriminator_maps_generated,
                                   discriminator_maps_real=discriminator_maps_real,
                                   loss_weights=self.config['loss_weights'])
-#       self.disc_values = disc_value
-#       self.disc_names = disc_names
         return loss
 
 
@@ -128,7 +126,7 @@ def train(config, generator, discriminator, kp_extractor, checkpoint, log_dir, d
                 generator_loss_values = [val.detach().cpu().numpy() for val in loss_values]
 
                 loss = sum(loss_values) 
-                loss.backward(retain_graph=not config['model_params']['detach_kp_discriminator'])                      
+                loss.backward(retain_graph=not config['model_params']['detach_kp_discriminator'])               
                 optimizer_generator.step()
                 optimizer_generator.zero_grad()
                 optimizer_discriminator.zero_grad()